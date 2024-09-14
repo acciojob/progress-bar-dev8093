ceba5282a8951af70d4ad9ba1f5f9df334c71433
Dev 8093 <168810708+dev8093@users.noreply.github.com>
2024-09-14 10:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+
+let circ=document.querySelectorAll(".circle")
+let prev=document.querySelector("#prev");
+let next=document.querySelector("#next");
+let line=document.querySelectorAll(".line")
+
+prev.disabled=true
+
+let i=0,j=0;
+
+
+ circ[i].classList.add("active");
+next.addEventListener("click",gonext)
+prev.addEventListener("click",goprev)
+
+function gonext(e){
+	
+ 	i++
+	if(i>=1){
+		prev.disabled=false
+	}
+	circ[i].classList.add("active");
+	line[j].classList.add("change");
+	console.log(i)
+	if(i==4){
+		next.disabled=true
+	}
+	j++
+	
+}
+function goprev(e) {
+	j--;
+	
+	circ[i].classList.remove("active");
+	line[j].classList.remove("change");
+	
+	i--;
+	
+	if(i<4){
+		next.disabled=false
+	}
+	//if(i==1){
+	if(i==0){
+		prev.disabled=true
+	}
+	
+	
+}
